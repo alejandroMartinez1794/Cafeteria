59d976582cc96638461e78964ac213d0d2edfc07
Alejandro Martinez <lamartinezf@libertadores.edu.co>
2023-08-16 23:22:35 -0500

    Ultimos cambios
    Cambios hasta la creacion de la seccion nosotros

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
+
+
 const {src, dest, watch, series, parallel} = require('gulp');
+
+// depedencias de CSS y SSASS 
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
-
+// depedencias de imagenes
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const webp= require('gulp-webp');
+const avif= require('gulp-avif');
 
 function css (done) {
    
@@ -14,12 +22,39 @@ function css (done) {
     done()    
 }
 
+function imagenes () {
+    return src ('./src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'))    
+}
+
+function versionWebp () {
+    const opciones = {
+        quality: 50
+    }
+    return src ('./src/img/**/*{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+}
+
+function versionAvif () {
+    const opciones = {
+        quality: 50
+    }
+    return src ('./src/img/**/*{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+}
+
 function dev (done) {
-    watch('./src/scss/**/*.scss', css)
-    
+    watch('./src/scss/**/*.scss', css);
+    watch('./src/img/**/*', imagenes);    
     done()
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
